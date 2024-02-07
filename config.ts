bf97eab39ca4f7f86234c15291ba6cb5018dcaa0
pranay213 <pranaykodam.213@gmail.com>
2024-02-07 10:09:07 +0530

    update

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export default {
   //BRM_URL: "http://localhost:3002/crm",
-  BRM_URL: 'https://crmapi.magicalcasino.co/crm',
-  ADMIN_URL: 'https://api.magicalcasino.co',
+  BRM_URL: 'https://crmapi.egamesplay.com/crm',
+
   //BRM_URL: "http://brmdev.duoex.com/brm"  // Local: http://localhost:3002/brm / Development: https://brmdev.casinocoin.org/brm / Production: https://api.casinocoin.org/1.0.0/brm
 };
